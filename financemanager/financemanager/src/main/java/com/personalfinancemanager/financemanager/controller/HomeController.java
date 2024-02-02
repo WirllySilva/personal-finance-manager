fbba29096ee2481ac7020d2059a1fdc17da3feb5
Wirlly Silva <126121325+WirllySilva@users.noreply.github.com>
2024-02-02 19:51:01 -0300

    The workspace  was setted up

--- financemanager/financemanager/src/main/java/com/personalfinancemanager/financemanager/controller/HomeController.java
+++ financemanager/financemanager/src/main/java/com/personalfinancemanager/financemanager/controller/HomeController.java
@@ -1,14 +1,28 @@
 package com.personalfinancemanager.financemanager.controller;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class HomeController {
 
-    @GetMapping("/")  
-    public String home() {
-        return "index"; //Thi will look for a file called ""index.html", in the folders resources/static/templates
+    @GetMapping("path")
+    public ResponseEntity<String> home() throws IOException {
+        Resource resource = new ClassPathResource("static/subdiretorio/index.html");
+
+        try (Scanner scanner = new Scanner(resource.getInputStream(), StandardCharsets.UTF_8)) {
+            String content = scanner.useDelimiter("\\A").next();
+            return ResponseEntity.ok().contentType(MediaType.TEXT_HTML).body(content);
+        }
     }
-    
+
 }
